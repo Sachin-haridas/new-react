b10d2364e9ef6a1c9a1b6a578ca1d6c587254727
sachin-haridas <sachincusat123@gmail.com>
2024-10-27 11:53:33 +0530

    new file

---
+++ 02vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <h1>with vite </h1>
+  )
+}
+
+export default App
